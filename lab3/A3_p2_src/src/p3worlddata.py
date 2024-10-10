f7bcf3487674fa57448d4b43a0b55d7fcc41af2a
M.Beck <mackbeckgames23@gmail.com>
2024-10-09 20:53:36 -0600

    refactored deliveryworld and dome basic implementatino for p3deliveryagent

--- lab3/A3_p2_src/src/p3worlddata.py
+++ lab3/A3_p2_src/src/p3worlddata.py
@@ -1,4 +1,18 @@
 
 
 
-Room1_A, Room1_B, Room1_C, Room2_A, Room2_B, Room2_C = (0,1), (0,2), (0,3), (1,1), (1,2), (1,3)
+r1a, r1b, r1c, r2a, r2b, r2c = 'Room1_A', 'Room1_B', 'Room1_C', 'Room2_A', 'Room2_B', 'Room2_C'
+
+
+p3world={
+    r1a: {r1b: 1, r2a: 1},
+    r1b: {r1a: 1, r1c: 1},
+    r1c: {r1b: 1, r2c: 1},
+    r2a: {r1a: 1, r2b: 1},
+    r2b: {r2a: 1, r2c: 1},
+    r2c: {r1c: 1, r2b: 1}
+}
+
+
+
+
