2e7bee4db57bf1893bec51290fbf36b6c4819b4f
M.Beck <mackbeckgames23@gmail.com>
2024-10-25 09:34:21 -0600

    comment

--- lab4/task6.py
+++ lab4/task6.py
@@ -2,7 +2,7 @@ import random
 from agentsPrograms import DepthFirstSearchGraph, LimitedDepthFirstSearchGraph
 from agents import ProblemSolvingMazeAgentDFS, ProblemSolvingMazeAgentDLS
 from mazeGraphClass import mazeGraph
-from MazeProblemTask6 import MazeProblem
+from mazeProblemClass import MazeProblem
 from myMazeData import makeMaze, defineMazeActions, makeMazeTransformationModel
 
 # Create the maze graph
@@ -24,29 +24,13 @@ maze_problem = MazeProblem(initial_state, goal_state, maze_graph)
 # Create and run the DFS agent
 dfs_agent = ProblemSolvingMazeAgentDFS(initial_state, maze_graph, goal_state)
 dfs_solution = dfs_agent.search(maze_problem)
+print(sum(maze_problem.path_cost(0, 'A', dfs_solution[i], 'B') 
+                   for i in range(0, len(dfs_solution))))
 
 
-# Create and run the DLS agent with a limit of 10
+"""# Create and run the DLS agent with a limit of 10
 limit = 10
 dls_agent = ProblemSolvingMazeAgentDLS(initial_state, maze_graph, goal_state, limit)
 dls_solution = dls_agent.search(maze_problem)
 
-
-# Compare the performance
-if dfs_solution and dls_solution:
-    dfs_cost = sum(maze_problem.path_cost(0, dfs_solution[i], dfs_solution[i+1], dfs_solution[i+2]) 
-                   for i in range(0, len(dfs_solution) - 2, 2))
-    dls_cost = sum(maze_problem.path_cost(0, dls_solution[i], dls_solution[i+1], dls_solution[i+2]) 
-                   for i in range(0, len(dls_solution) - 2, 2))
-    
-    print(f"DFS path cost: {dfs_cost}")
-    print(f"DLS path cost: {dls_cost}")
-    
-    if dfs_cost < dls_cost:
-        print("Depth-First Search agent is more productive (spends less performance).")
-    elif dls_cost < dfs_cost:
-        print("Depth-Limited Search agent is more productive (spends less performance).")
-    else:
-        print("Both agents have the same performance.")
-else:
-    print("One or both agents failed to find a solution.")
+"""
