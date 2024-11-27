de9d8280cef3189a3ef591c30246b4ef11f060be
spectrejuni <spectregex@gmail.com>
2024-11-26 19:43:46 -0700

    Finished task 2 of part 1.

--- lab6/Part 1/algorithms.py
+++ lab6/Part 1/algorithms.py
@@ -39,21 +39,45 @@ def AC3(csp):
 
 def AC3v2(csp):
   queue = Queue()
-
+  
   print(f"Initial queue:")
   for Xi in csp.variables:
     for Xk in csp.neighbors[Xi]:
-
       queue.put((Xi, Xk))
       print((Xi, Xk), end=" ")
     print()
    
   csp.support_pruning()
   checks = 0
+
+  # Ensures that constraints have the backward direction to enforce arc-consistency.
+  temp = csp.constraints
+  for expression in temp:
+    A,Ex,B = expression.split(' ')
+    if Ex == '>':
+      Ex = '<'
+    elif Ex == '<':
+      Ex = '>'
+    else:
+      continue
+    newExpression = f'{B} {Ex} {A}'
+    if newExpression not in csp.constraints:
+      (csp.constraints).append(newExpression)
+
   while list(queue.queue):
     (Xi, Xj) = queue.get()
     #print(f'Arc {(Xi, Xj)} is cheking')
+
+    # Takes the constraint that matches (Xi, Xj) from the list of constraints and creates a temporary lambda function implementing it.
+    temp = csp.constraints
+    for expression in temp:
+      A,Ex,B = expression.split(' ')
+      if A == Xi and B == Xj:
+        csp.constraints = eval(f'lambda A, a, B, b: a {Ex} b')
+        break  
     revised, checks = revise(csp, Xi, Xj, checks)
+    csp.constraints = temp
+
     if revised:
       if not csp.curr_domains[Xi]:
         return False, checks  # CSP is inconsistent
@@ -62,18 +86,17 @@ def AC3v2(csp):
           queue.put((Xk, Xi))
     print(list(queue.queue))
 
-    print(f'Arc {(Xj, Xi)} is cheking')
+    '''print(f'Arc {(Xj, Xi)} is cheking')
     revised, checks1 = back_revise(csp, Xi, Xj, checks)
     if revised:
       if not csp.curr_domains[Xj]:
         return False, checks  # CSP is inconsistent
       for Xk in csp.neighbors[Xj]:
         if Xk != Xi:
-          queue.put((Xk, Xj))
+          queue.add((Xk, Xj))'''
 
       
   return True, checks  # CSP is satisfiable
-
 
 
 def revise(csp, Xi, Xj, checks=0):
@@ -85,7 +108,7 @@ def revise(csp, Xi, Xj, checks=0):
         # if all(not csp.constraints(Xi, x, Xj, y) for y in csp.curr_domains[Xj]):
         conflict = True
         #print(csp.curr_domains[Xj])
-        print(csp.curr_domains[Xi])
+        #print(csp.curr_domains[Xi])
         for y in csp.curr_domains[Xj]:
             if csp.constraints(Xi, x, Xj, y):
                 conflict = False
